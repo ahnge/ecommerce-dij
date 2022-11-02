73f349b4f81dcd6328c5068423e96c2d41eff175
ahnge <ahnge226@gmail.com>
2022-11-02 10:49:29 +0630

    finish up showing dynamic ui

--- store/views.py
+++ store/views.py
@@ -25,5 +25,13 @@ def cart(req):
 
 
 def checkout(req):
-    context = {}
+    if req.user.is_authenticated:
+        customer = req.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+
+    context = {"items": items, "order": order}
     return render(req, "store/checkout.html", context)
